b4b5e34d9c4ddf76bbb2e35bbe8a5f713c9887c6
agileup <agileup@gmail.com>
2019-03-05 02:27:50 +0900

    feat(): implement base layout

--- lib/main.dart
+++ lib/main.dart
@@ -21,14 +21,124 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  var appColors = [
+    Color.fromRGBO(231, 129, 109, 1.0),
+    Color.fromRGBO(99, 138, 223, 1.0),
+    Color.fromRGBO(111, 194, 173, 1.0)
+  ];
+  var cardIndex = 0;
+
+  var currentColor = Color.fromRGBO(231, 129, 109, 1.0);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: currentColor,
       appBar: AppBar(
-        title: Text('TODO'),
+        title: Text(
+          'TODO',
+          style: TextStyle(fontSize: 16.0),
+        ),
+        backgroundColor: appColors[cardIndex],
         centerTitle: true,
+        actions: <Widget>[
+          Padding(
+            padding: const EdgeInsets.only(right: 16.0),
+            child: Icon(Icons.search),
+          )
+        ],
+        elevation: 0.0,
       ),
-      body: Center(),
+      body: Center(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Row(),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 64.0, vertical: 32.0),
+              child: Container(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Padding(
+                      padding: EdgeInsets.symmetric(vertical: 8.0),
+                      child: Icon(Icons.account_circle,
+                          size: 45.0, color: Colors.white),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.fromLTRB(0.0, 16.0, 0.0, 12.0),
+                      child: Text(
+                        'Hello, Jane.',
+                        style: TextStyle(fontSize: 30.0, color: Colors.white),
+                      ),
+                    ),
+                    Text(
+                      'Looks like feel good.',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    Text(
+                      'You have 3 tasks to do today',
+                      style: TextStyle(color: Colors.white),
+                    )
+                  ],
+                ),
+              ),
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Padding(
+                  padding:
+                      EdgeInsets.symmetric(horizontal: 64.0, vertical: 16.0),
+                  child: Text(
+                    'TODAY ---',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+                Container(
+                  height: 350.0,
+                  child: ListView.builder(
+                      physics: NeverScrollableScrollPhysics(),
+                      itemCount: 3,
+//                      controller: scrollController,
+                      scrollDirection: Axis.horizontal,
+                      itemBuilder: (context, position) {
+                        return GestureDetector(
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Card(
+                              child: Container(
+                                width: 250.0,
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: <Widget>[
+                                    Padding(
+                                      padding: EdgeInsets.all(8.0),
+                                      child: Row(),
+                                    ),
+                                    Padding(
+                                      padding: EdgeInsets.all(8.0),
+                                      child: Row(),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(10.0),
+                              ),
+                            ),
+                          ),
+                        );
+                      }),
+                )
+              ],
+            ),
+          ],
+        ),
+      ),
+      drawer: Drawer(),
     );
   }
 }
